24ea6570b2a8e9b6d410fe13ad4cbb19ba725672
lcenriquez <luiscarlos_enriquez@me.com>
2023-05-10 06:43:35 -0600

    programmatically open and close side-drawer

--- src/components.d.ts
+++ src/components.d.ts
@@ -22,6 +22,7 @@ export namespace Components {
     }
     interface SideDrawer {
         "heading": string;
+        "open": boolean;
     }
 }
 declare global {
@@ -59,6 +60,7 @@ declare namespace LocalJSX {
     }
     interface SideDrawer {
         "heading"?: string;
+        "open"?: boolean;
     }
     interface IntrinsicElements {
         "my-component": MyComponent;
--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -9,12 +9,14 @@ import { Component, Prop, h } from '@stencil/core';
 // Stencil extends HTMLElement for us :D
 export class SideDrawer {
   @Prop({ reflect: true }) heading: string; // reflect makes the html attribute change when modified programmatically
+  @Prop({ mutable: true, reflect: true }) open: boolean; // we need to set mutable to true if we want to change an attribute value from within the component itself
 
   render() {
     return (
       <aside>
         <header>
           <h1>{this.heading}</h1>
+          <button onClick={() => (this.open = false)}>x</button>
         </header>
         <slot></slot>
       </aside>
